be3d505a07670b30a1ba424189cff162b454d21f
Rahul Gupta <rahul@kuttl.in>
2023-12-06 20:39:38 +0530

    modelSchema change for payload structure

--- app-read-email/model/emailModel.js
+++ app-read-email/model/emailModel.js
@@ -1,21 +1,28 @@
 import mongoose from 'mongoose';
 
+// Modified message schema to include headers, body, and attachment
 const messageSchema = new mongoose.Schema({
   messageID: { type: String, unique: true },
-  title: String,
+  headers: {
+    'Message-ID': String,
+    Subject: String,
+    deviceId: String,
+    From: String,
+    To: String,
+    'date-time': String
+  },
+  body: String,
+  attachment: [String] // Assuming attachments are represented as an array of strings (e.g., file paths or URLs)
 });
 
 const threadSchema = new mongoose.Schema({
   threadID: { type: String, required: true },
-  messages: [messageSchema],
+  messages: [messageSchema]
 });
 
+// Removing sender_details from payloadSchema as it's not present in the new JSON format
 const payloadSchema = new mongoose.Schema({
-  sender_details: {
-    email: String,
-    deviceId: String,
-  },
-  threads: [threadSchema],
+  threads: [threadSchema]
 });
 
 const PayloadModel = mongoose.model('Payload', payloadSchema);
